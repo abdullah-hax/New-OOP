14fd561787dc93fc3dd3017323789c9d6de145f7
Md Abdullah (Linux) <oceanh6672@gmail.com>
2026-01-27 11:05:45 +0600

    interface confusion clear

--- Interface.java
+++ Interface.java
@@ -1,10 +1,11 @@
+// Interface, inheretance না
+
 package newOOP;
 
 interface Payment {
-    void pay(double amount);  // what to do : pay krte hbe
-                              // how to do : bole deya nai
+    void pay(double amount); // what to do : pay krte hbe
+                             // how to do : bole deya nai
 
-    
 }
 
 class Bkash implements Payment {
@@ -20,16 +21,24 @@ class Card implements Payment {
 }
 
 public class Interface {
-    
-}
 
+}
 
 /* ✔ Interface ব্যবহার করবে যখন:
 
-শুধু ability/capability define করতে চাও
-class গুলোর মধ্যে “is-a” সম্পর্ক নাই
-code share করার দরকার নাই */
+    শুধু ability/capability define করতে চাও
+    class গুলোর মধ্যে “is-a” সম্পর্ক নাই
+    code share করার দরকার নাই */
 
-
-/* Interface → only contract  (karon only abstract class ache)
+/* Interface → only contract (ja method dibo sb implement krte hobe, karon only abstract class ache)
 Abstract class → contract + partial implementation (normal class)*/
+
+
+/* ৫️⃣ তাহলে “class গুলোর মধ্যে is-a নাই” কথাটার মানে কী?
+
+Class গুলো একই পরিবারভুক্ত না,
+কিন্তু একই capability share করে
+
+তাই:
+    abstract class ❌ (family লাগে)
+    interface ✅ (capability লাগে) */
--- info.java
+++ info.java
@@ -16,3 +16,28 @@ engine1 = new Engine();
     → reference এখন সেই object-কে point করছে */
 
 //-------------------------------------------------------
+
+/* - extends means প্রসারিত করা , তবে java তে অন্তর্ভুক্ত অর্থে ব্যবহৃত হবে 
+
+        class Bird extends Animal
+        class Toyota extends Car 
+        
+        =>  Bird, Animal এর অন্তর্ভুক্ত 
+            তাই Animal এর বৈশিষ্ট্যগুলো inheretantly পাবে
+            is a সম্পর্ক আছে
+        
+   - implements means বাস্তবায়ন করা
+
+        class Smartphone implements Camera, GPS 
+        class Nagad implements Payment, Transfer  
+        
+        => Nagad, Transfer & Payment করতে সক্ষম 
+           Smartphone, Camera & GPS এর কাজগুলো করতে সক্ষম 
+           
+        example: class Bank implements Transfer, SaveGold
+                 class Uber implements Riding
+                 Father implements Business, Cooking, Journey 
+                 
+                (Listening -> Shadowing -> //@ Juggling)
+
+        Interface, inheretance না  */
